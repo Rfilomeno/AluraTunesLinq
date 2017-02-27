f2d1f41cb7755ed1ad5359ddb9f6d1c814a7ce34
rodrigo filomeno <rodrigo.filomeno@al.infnet.edu.br>
2017-02-27 06:29:21 -0300

    aula 2 refactory

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -11,20 +11,7 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
-			XElement root = XElement.Load(@"Data\AluraTunes.xml");
-			var query = from g in root.Element("Generos").Elements("Genero")
-						join m in root.Element("Musicas").Elements("Musica")
-							on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
-						select new
-						{
-							MusicaId = m.Element("MusicaId").Value,
-							Musica = m.Element("Nome").Value,
-							Genero = g.Element("Nome").Value
-						};
-			foreach (var item in query)
-			{
-				Console.WriteLine("{0}\t{1}",item.Musica, item.Genero);
-			}
+			
         }
 
         
